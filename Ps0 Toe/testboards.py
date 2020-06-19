548167cc80b4aae4174791fcf7d11067eb59f15d
Kirk Sripinyo <kirk.sripinyo@gmail.com>
2020-06-19 05:27:13 -0400

    Implemented terminal and utility function.

---
+++ Ps0 Toe/testboards.py
@@ -0,0 +1,13 @@
+boardwon1 = [[X,X,X],
+            [O,X,EMPTY],
+            [O,O,EMPYY]]
+
+boardwon2 = [[X,X,EMPTY],
+            [O,X,EMPTY],
+            [O,O,X]]
+
+tboard1 = [[EMPTY,EMPTY,EMPTY],
+            [EMPTY,EMPTY,EMPTY],
+            [EMPTY,EMPTY,EMPTY]]
+
+
